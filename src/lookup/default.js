e4bde6815e1f0b3f8a42908bde9c776aeb08c5a0
benpptung <benpptung@tacol.biz>
2019-08-14 14:54:08 +0800

    change locale tag format to hyphen notation

--- src/lookup/default.js
+++ src/lookup/default.js
@@ -1,4 +1,4 @@
-const createDefaultLookup = (locale = 'en_GB') => {
+const createDefaultLookup = (locale = 'en-GB') => {
   const invalidLocaleError = new Error('A valid locale is required for default lookup');
 
   if (typeof locale !== 'string') {
